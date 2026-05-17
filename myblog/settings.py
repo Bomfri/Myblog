f36603ac9c8c70353bd2c97a1ab8b3fa9a0b1386
Bomfri <bardiabayat223@gmail.com>
2026-05-16 18:57:34 -0700

    Hide secret key using environment variables

--- myblog/settings.py
+++ myblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=g5fb*c2k8yl8lu#+j_z@nq8^xov5b3oqyia!_u)3guksw&7tg'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
